f3daf48b18dac5ae14b4313353f0072764e24324
Jonathan <30329245+CookStar@users.noreply.github.com>
2020-09-22 00:38:38 +0900

    Updated DataTools.

--- addons/source-python/packages/custom/datatools/__init__.py
+++ addons/source-python/packages/custom/datatools/__init__.py
@@ -3,10 +3,20 @@
 """Provides Data Tools based functionality."""
 
 # =============================================================================
+# >> FORWARD IMPORTS
+# =============================================================================
+# Data Tools Imports
+#   Base
+from .base import get_value_pointer
+from .base import set_data_from_file
+from .base import set_value_pointers_from_file
+
+
+# =============================================================================
 # >> ALL DECLARATION
 # =============================================================================
-__all__ = ("set_data_from_file",
+__all__ = ("get_value_pointer",
+           "set_data_from_file",
            "set_value_pointers_from_file",
-           "value_pointer",
            )
 
--- addons/source-python/packages/custom/datatools/base.py
+++ addons/source-python/packages/custom/datatools/base.py
@@ -19,7 +19,7 @@ from memory.helpers import parse_data
 # =============================================================================
 # >> FUNCTIONS
 # =============================================================================
-def value_pointer(binary, identifier, offset=0, level=0, srv_check=True):
+def get_value_pointer(binary, identifier, offset=0, level=0, srv_check=True):
     """Return the value pointers."""
     # Get the binary
     binary = find_binary(binary, srv_check)
@@ -49,7 +49,7 @@ def set_value_pointers_from_file(cls, f):
 
     # Create the value pointer attribute
     for name, data in pointers:
-        setattr(cls, name, value_pointer(*data))
+        setattr(cls, name, get_value_pointer(*data))
 
 def set_data_from_file(cls, f):
     """Registers a new data from a file."""
@@ -59,4 +59,6 @@ def set_data_from_file(cls, f):
     ))
 
     for name, value in cls_dict.items():
-        setattr(cls, name, value)
+        if not name.startswith("_"):
+            setattr(cls, name, value)
+
