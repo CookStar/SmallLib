f339f05d7fcd69eb0a99826d5a8b30faf596bbf6
Jonathan <30329245+CookStar@users.noreply.github.com>
2020-10-25 08:49:05 +0900

    Added Ctype to DataTools.

---
+++ addons/source-python/packages/custom/datatools/ctypes.py
@@ -0,0 +1,37 @@
+# ../ctypes.py
+
+"""Provides a helper feature for ctype functions."""
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python Imports
+#   Memory
+from memory.manager import manager
+
+# Memory Tools Imports
+#   Conventions
+from memorytools.ctypes import get_ctype_function
+
+
+# =============================================================================
+# >> ALL DECLARATION
+# =============================================================================
+__all__ = ("create_ctype_pipe_from_file",
+           )
+
+
+# =============================================================================
+# >> FUNCTIONS
+# =============================================================================
+def create_ctype_pipe_from_file(file, auto_dealloc=True):
+    pipe = manager.create_pipe_from_file(file)
+    cls_dict = {}
+    for name in [attr for attr in pipe.__dict__.keys() if not attr.startswith("__")]:
+        if auto_dealloc:
+            cls_dict[name] = get_ctype_function(getattr(pipe, name))
+        else:
+            cls_dict[name] = get_ctype_function(getattr(pipe, name), auto_dealloc=False).ctype
+
+    return type("CtypePipe", (object,), cls_dict)
+
