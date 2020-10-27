3f423194d7e6e20520a84af8f6906468b00fde56
Jonathan <30329245+CookStar@users.noreply.github.com>
2020-10-27 17:47:07 +0900

    Updated Net Channel Handler listener.

--- addons/source-python/packages/custom/nch_listeners/__init__.py
+++ addons/source-python/packages/custom/nch_listeners/__init__.py
@@ -6,7 +6,6 @@
 # >> FORWARD IMPORTS
 # =============================================================================
 # Net Channel Handler Listeners Imports
-from nch_listeners.listeners import NetChannelHandlerLMD
 from nch_listeners.listeners import OnConnectionStart
 from nch_listeners.listeners import OnConnectionStop
 from nch_listeners.listeners import OnConnectionClosing
@@ -18,24 +17,23 @@ from nch_listeners.listeners import OnFileReceived
 from nch_listeners.listeners import OnFileDenied
 from nch_listeners.listeners import OnFileSent
 from nch_listeners.listeners import OnChangeSplitscreenUser
-from nch_listeners.listeners import on_connection_start_listener_manager
-from nch_listeners.listeners import on_connection_stop_listener_manager
-from nch_listeners.listeners import on_connection_closing_listener_manager
-from nch_listeners.listeners import on_connection_crashed_listener_manager
-from nch_listeners.listeners import on_packet_start_listener_manager
-from nch_listeners.listeners import on_packet_end_listener_manager
-from nch_listeners.listeners import on_file_requested_listener_manager
-from nch_listeners.listeners import on_file_received_listener_manager
-from nch_listeners.listeners import on_file_denied_listener_manager
-from nch_listeners.listeners import on_file_sent_listener_manager
-from nch_listeners.listeners import on_change_splitscreen_user_listener_manager
+from nch_listeners.manager import on_connection_start_listener_manager
+from nch_listeners.manager import on_connection_stop_listener_manager
+from nch_listeners.manager import on_connection_closing_listener_manager
+from nch_listeners.manager import on_connection_crashed_listener_manager
+from nch_listeners.manager import on_packet_start_listener_manager
+from nch_listeners.manager import on_packet_end_listener_manager
+from nch_listeners.manager import on_file_requested_listener_manager
+from nch_listeners.manager import on_file_received_listener_manager
+from nch_listeners.manager import on_file_denied_listener_manager
+from nch_listeners.manager import on_file_sent_listener_manager
+from nch_listeners.manager import on_change_splitscreen_user_listener_manager
 
 
 # =============================================================================
 # >> ALL DECLARATION
 # =============================================================================
-__all__ = ("NetChannelHandlerLMD",
-           "OnConnectionStart",
+__all__ = ("OnConnectionStart",
            "OnConnectionStop",
            "OnConnectionClosing",
            "OnConnectionCrashed",
