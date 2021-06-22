0e618878a8feba2969be25591674dfcbb7aa8960
Jonathan <30329245+CookStar@users.noreply.github.com>
2021-06-22 22:48:24 +0900

    Added various functions to MemoryTools.

--- addons/source-python/packages/custom/memorytools/__init__.py
+++ addons/source-python/packages/custom/memorytools/__init__.py
@@ -5,28 +5,41 @@
 # =============================================================================
 # >> IMPORTS
 # =============================================================================
+# Python Imports
+#   Ctypes
+from ctypes import c_ubyte
+from ctypes import c_void_p
+from ctypes import memmove
+
 # Source.Python Imports
 #   Memory
+from memory import alloc
 from memory import find_binary
 from memory import BinaryFile
 from memory import Pointer
-from memory.helpers import Array
-from memory.manager import manager
+#from memory.helpers import Array
+#from memory.manager import manager
 from memory.manager import Type
 
 
 # =============================================================================
 # >> ALL DECLARATION
 # =============================================================================
-__all__ = ("get_jmp_address",
+__all__ = ("get_bytes",
+           "get_call_bytes",
+           "get_jmp_address",
            "get_jmp_bytes",
            "get_jmp_short_address",
            "get_offset",
            "get_pointer",
            "get_relative_pointer",
            "get_relative_pointer_from_pointer",
+           "load_binary",
            "mem_print",
            "mem_write",
+           "set_bytes",
+           "set_global_addresses",
+           "set_local_addresses",
            )
 
 
@@ -91,6 +104,31 @@ def get_relative_pointer_from_pointer(pointer, offset, size=4):
 
     return pointer
 
+def set_local_addresses(pointer, value, offsets):
+    if isinstance(pointer, int):
+        pointer = Pointer(pointer)
+
+    value = int(value)
+    for offset in offsets:
+        pointer.set_uint(value, offset)
+
+def set_global_addresses(pointer, offsets):
+    if isinstance(pointer, int):
+        pointer = Pointer(pointer)
+
+    address = int(pointer)
+    for offset in offsets:
+        pointer.set_uint(pointer.get_uint(offset) + address, offset)
+
+def get_bytes(pointer, length, offset=0):
+    return bytes((c_ubyte*length).from_address(int(pointer)+offset))
+
+def set_bytes(pointer, data, offset=0):
+    if isinstance(data, bytearray):
+        data = bytes(data)
+
+    memmove(c_void_p(int(pointer)+offset), data, len(data))
+
 def get_jmp_address(base, dest):
     base_address = int(base)
     dest_address = int(dest)
@@ -109,6 +147,9 @@ def get_jmp_short_address(base, dest):
 
     return None
 
+def get_call_bytes(base, dest):
+    return b"\xe8"+((get_jmp_address(base, dest)).to_bytes(4, "little"))
+
 def get_jmp_bytes(base, dest, short=True):
     if short:
         jmp_address = get_jmp_short_address(base, dest)
@@ -117,12 +158,30 @@ def get_jmp_bytes(base, dest, short=True):
 
     return b"\xe9"+((get_jmp_address(base, dest)).to_bytes(4, "little"))
 
-def mem_print(pointer, length):
-    data = Array(manager, False, Type.UCHAR, pointer, length)
+def load_binary(path, auto_dealloc=True):
+    with open(path, "rb") as file:
+        data = file.read()
+
+    length = len(data)
+
+    binary = alloc(length, auto_dealloc)
+    binary.unprotect(length)
+
+    memmove(c_void_p(binary.address), data, length)
+
+    return binary, length
+
+def mem_print(pointer, length, offset=0):
+    #data = Array(manager, False, Type.UCHAR, pointer, length)
+    #print(' '.join("{:02X}".format(i) for i in data))
+    data = get_bytes(pointer, length, offset)
     print(' '.join("{:02X}".format(i) for i in data))
 
-def mem_write(path, pointer, length):
-    data = Array(manager, False, Type.UCHAR, pointer, length)
+def mem_write(path, pointer, length, offset=0):
+    #data = Array(manager, False, Type.UCHAR, pointer, length)
+    #with open(path, "wb") as file:
+    #    file.write(bytes([i for i in data]))
+    data = get_bytes(pointer, length, offset)
     with open(path, "wb") as file:
-        file.write(bytes([i for i in data]))
+        file.write(data)
 
