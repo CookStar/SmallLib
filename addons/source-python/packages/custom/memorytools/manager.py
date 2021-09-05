e04da430a36d994653ce960614d89cc21924e282
Jonathan <30329245+CookStar@users.noreply.github.com>
2021-09-05 14:58:15 +0900

    Reverted the ability of get_function_from_dict to read entity data.

--- addons/source-python/packages/custom/memorytools/manager.py
+++ addons/source-python/packages/custom/memorytools/manager.py
@@ -196,20 +196,6 @@ def get_function_from_dict(raw_data, manager=manager):
         ptr = get_relative_pointer(*data[:5])
         function_dict[name] = manager.pipe_function_pointer(ptr, *data[5:])
 
-    # Remove unnecessary items.
-    for method in ("instance_attribute",
-                   "pointer_attribute",
-                   "static_instance_array",
-                   "dynamic_instance_array",
-                   "static_pointer_array",
-                   "dynamic_pointer_array",
-                   "virtual_function",
-                   "binary_attribute",
-                   "binary_array",
-                   "binary_virtual_function",
-                   "binary_pointer"):
-        raw_data.pop(method, None)
-
     # Prepare functions
     funcs = parse_data(
         manager,
