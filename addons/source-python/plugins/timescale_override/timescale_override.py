c1953688f10cd34f271b4fede4c5ac0c77a7c3be
Jonathan <30329245+CookStar@users.noreply.github.com>
2022-11-09 07:37:28 +0900

    Updated Timescale Override data.
    Removed CHEAT Flag of host_timescale in Timescale Override.

--- addons/source-python/plugins/timescale_override/timescale_override.py
+++ addons/source-python/plugins/timescale_override/timescale_override.py
@@ -8,12 +8,22 @@
 # Source.Python Imports
 #   Core
 from core import PLATFORM
+#   Cvars
+from cvars import cvar
+from cvars.flags import ConVarFlags
 #   Paths
 from paths import PLUGIN_DATA_PATH
 
 # Memory Tools Imports
 #   Memory Tools
 from memorytools.manager import create_patchers_from_file
+
+
+# =============================================================================
+# >> CONVARS
+# =============================================================================
+host_timescale = cvar.find_var("host_timescale")
+host_timescale.flags &= ~ConVarFlags.CHEAT
 
 
 # =============================================================================
